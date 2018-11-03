e38fd801e83fac4ee8070b9229ff10a209716dc0
Puratinamu <26533316+Puratinamu@users.noreply.github.com>
2018-11-02 20:18:24 -0400

    sound command changes

--- commands/sound.js
+++ commands/sound.js
@@ -3,12 +3,10 @@ const config = require('../config.json');
 module.exports = {
     execute: function (args, message, logger) {
         let connection = message.guild.voiceConnection;
-        let playing = false;
         if (!connection) {
             message.channel.send('Use !join to let me join a voice channel first.')
                 .catch(error => logger.error(error));
-        } else if (!playing) {
-            playing = true;
+        } else if (!connection.dispatcher) {
             let song_name = '';
             for (let i = 1; i < args.length; i++) {
                 song_name += (args[i] + ' ');
@@ -25,39 +23,37 @@ module.exports = {
             });
             dispatcher.on('end', reason => logger.info('Stopped playing on ' + message.channel.name + ' because ' + reason));
             let collector = message.channel.createCollector(m => m);
-            let paused = false;
             collector.on('collect', m => {
                 if (m.content.startsWith(config.prefix) && !m.author.bot) {
                     let sound_args = m.content.substring(1).split(' ');
                     let sound_cmd = sound_args[0];
                     switch (sound_cmd) {
                         case 'pause':
-                            if (playing && !paused) {
+                            if (!dispatcher.paused) {
                                 m.channel.send('Sound paused.')
                                     .catch(error => logger.error(error));
                                 dispatcher.pause();
-                                paused = true;
                                 logger.info(m.author.username + ' paused sound');
                             }
                             break;
                         case 'resume':
-                            if (playing && paused) {
+                            if (dispatcher.paused) {
                                 m.channel.send('Sound resumed.')
                                     .catch(error => logger.error(error));
                                 dispatcher.resume();
-                                paused = false;
                                 logger.info(m.author.username + ' resumed sound');
                             }
                             break;
                         case 'stop':
-                            if (playing) {
+                            if (dispatcher) {
                                 m.channel.send('Stopping sound.')
                                     .catch(error => logger.error(error));
                                 dispatcher.end();
-                                playing = false;
+                                dispatcher.destroy();
                                 logger.info(m.author.username + ' stopped sound');
+                                collector.destroy();
+                                break;
                             }
-                            break;
                     }
                 }
             });
